4b0bda842b6fa681f6637e428c2b938354a7d21c
Marco Ravelo <mravelo5874@gmail.com>
2025-08-02 14:02:37 -0500

    added audio manager and tracks in engine

---
+++ scripts/audio_manager.gd
@@ -0,0 +1,50 @@
+extends Node
+
+var bass_active: bool = false
+var guitar_active: bool = false
+var piano_active: bool = false
+var violins_active: bool = false
+var trumpts_active: bool = false
+var kazoo_active: bool = false
+
+# for testing - control the separate audio tracks w/ number keys
+func _input(event):
+	if Input.is_key_pressed(KEY_1):
+		bass_active = !bass_active
+		print("bass track: %s" % [bass_active])
+	if Input.is_key_pressed(KEY_2):
+		guitar_active = !guitar_active
+		print("guitar track: %s" % [guitar_active])
+	if Input.is_key_pressed(KEY_3):
+		piano_active = !piano_active
+		print("piano track: %s" % [piano_active])
+	if Input.is_key_pressed(KEY_4):
+		violins_active = !violins_active
+		print("violins track: %s" % [violins_active])
+	if Input.is_key_pressed(KEY_5):
+		trumpts_active = !trumpts_active
+		print("trumpets track: %s" % [trumpts_active])
+	if Input.is_key_pressed(KEY_6):
+		kazoo_active = !kazoo_active
+		print("kazoo track: %s" % [kazoo_active])
+	update_audio_busses()
+		
+func update_audio_busses():
+	# bass bus
+	var bass_index = AudioServer.get_bus_index("BassBus")
+	AudioServer.set_bus_volume_db(bass_index, 11 if bass_active else -80.0)
+	# bass bus
+	var guitar_index = AudioServer.get_bus_index("GuitarBus")
+	AudioServer.set_bus_volume_db(guitar_index, 0.0 if guitar_active else -80.0)
+	# piano bus
+	var piano_index = AudioServer.get_bus_index("PianoBus")
+	AudioServer.set_bus_volume_db(piano_index, 0.0 if piano_active else -80.0)
+	# violins bus
+	var violins_index = AudioServer.get_bus_index("ViolinsBus")
+	AudioServer.set_bus_volume_db(violins_index, 0.0 if violins_active else -80.0)
+	# trumpets bus
+	var trumpets_index = AudioServer.get_bus_index("TrumpetsBus")
+	AudioServer.set_bus_volume_db(trumpets_index, 0.0 if trumpts_active else -80.0)
+	# kazoo bus
+	var kazoo_index = AudioServer.get_bus_index("KazooBus")
+	AudioServer.set_bus_volume_db(kazoo_index, 0.0 if kazoo_active else -80.0)
